d57ac2c9c05c2bffe8707601b1ff60db41e18d4f
nack43 <gurin140@gmail.com>
2018-11-17 17:07:21 +0900

    Add port to deal with heroku

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(env.process.PORT || port, () => console.log(`Example app listening on port ${port}!`));
