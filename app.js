03f2dffc853cf047b17dde35a02fb25e1a87ce1f
nack43 <gurin140@gmail.com>
2018-11-18 17:36:32 +0900

    Install and set line sdk

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const line = require('@line/bot-sdk');
 const app = express();
+
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+app.get('/', line.middleware(config), (req, res) => {
   console.log(req.body)
   res.sendStatus(200)
 })
